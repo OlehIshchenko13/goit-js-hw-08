a501ff542a3d27314b2fb557be5d7d7905a0e4f8
Oleh <97728959+OlehIshchenko13@users.noreply.github.com>
2022-11-09 21:25:48 +0200

    d

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,22 +1,7 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-// const iframe = document.querySelector('iframe');
-// const player = new Player(iframe);
 
-// player.on('timeupdate', throttle(function (date) {
-// 	localStorage.setItem("videoplayer-current-time", date.seconds);
-// }, 1000));
-// const currentTime = Number(localStorage.getItem("videoplayer-current-time"))
-
-// player.setCurrentTime(currentTime).then(function (seconds) {}).catch(function (error) {
-// 	switch (error.name) {
-// 		case 'RangeError':
-// 			break;
-// 		default:	
-// 			break;
-// 	}
-// });
 const iframe = document.querySelector('iframe');
     const player = new Player(iframe);
 
